05e4837bc85e4476883256c1208c27f469ba7164
1508474781@qq.com <ys@DESKTOP-CSSQOQL>
2018-04-12 17:31:13 +0800

    00

---
+++ xiakan/src/main/java/com/spider/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.spider.dao;
+
+import com.spider.model.User;
+
+public interface UserDao {
+ 
+    User queryById(int id);
+}
---
+++ xiakan/src/main/java/com/spider/service/Springtest.java
@@ -0,0 +1,9 @@
+package com.spider.service;
+
+public class Springtest {
+
+	public static void main(String[] args) {
+ 
+	}
+
+}
