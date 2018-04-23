a98c10fc7043962364e76fcc0b7aa271e2d5f529
yansheng <1508474781@qq.com>
2018-04-23 17:39:03 +0800

    clear

--- xiakan/src/main/java/com/spider/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.spider.dao;
-
-import com.spider.model.User;
-
-public interface UserDao {
- 
-    User queryById(int id);
-}
--- xiakan/src/main/java/com/spider/service/Springtest.java
+++
@@ -1,9 +0,0 @@
-package com.spider.service;
-
-public class Springtest {
-
-	public static void main(String[] args) {
- 
-	}
-
-}
